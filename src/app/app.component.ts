5f9e10e6f29826186fe0c5c364c33000b0ff7cc1
Vinicius Silva <vinicius768@msn.com>
2018-12-30 15:34:45 -0200

    Fixed HTTP fetch to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StarWarsService } from './star-wars.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'course-project';
+  swService: StarWarsService;
+
+  constructor(swService: StarWarsService) {
+    this.swService = swService;
+  }
+
+  ngOnInit() {
+    this.swService.fetchCharacters();
+  }
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -20,8 +20,6 @@ export class ListComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.swService.fetchCharacters();
-
     this.activatedRoute.params.subscribe((params) => {
       this.characters = this.swService.getCharacters(params.side);
       this.loadedSide = params.side;
